f634d11dd3d871cfc7eaa460468817ba3746e809
mjyplusone <1546331221@qq.com>
2017-11-16 11:15:55 +0800

    food商品详情页实现-评价列表

--- sell/src/components/food/food.vue
+++ sell/src/components/food/food.vue
@@ -33,6 +33,21 @@
                 <div class="rating">
                     <h1 class="title">商品评价</h1>
                     <ratingselect @select="selectRating" @toggle="toggleContent" :select-type="selectType" :only-content="onlyContent" :desc="desc" :ratings="food.ratings"></ratingselect>
+                    <div class="rating-wrapper">
+                        <ul v-show="food.ratings && food.ratings.length">
+                            <li v-show="needShow(rating.rateType, rating.text)" v-for="rating in food.ratings" class="rating-item border-1px">
+                                <div class="user">
+                                    <span class="name">{{ rating.username }}</span>
+                                    <img class="avatar" width="12" height="12" :src="rating.avatar" alt="">
+                                </div>
+                                <div class="time">{{ rating.rateTime | formatDate }}</div>
+                                <p class="text">
+                                    <span :class="{'icon-thumb_up': rating.rateType === 0, 'icon-thumb_down': rating.rateType === 1}"></span>{{ rating.text }}
+                                </p>
+                            </li>
+                        </ul>
+                        <div class="no-rating" v-show="!food.ratings || !food.ratings.length">暂无评价</div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -45,6 +60,7 @@
     import Vue from 'vue';
     import split from 'components/split/split.vue';
     import ratingselect from 'components/ratingselect/ratingselect.vue';
+    import {formatDate} from '../../common/js/date.js';
 
     // const POSITIVE = 0;
     // const NEGATIVE = 1;
@@ -67,6 +83,12 @@
                     negative: '吐槽'
                 }
             };
+        },
+        filters: {
+            formatDate(time) {
+                let date = new Date(time);
+                return formatDate(date, 'yyyy-MM-dd hh:mm');
+            }
         },
         methods: {
             show() {
@@ -104,6 +126,16 @@
                 this.$nextTick(() => {
                     this.scroll.refresh();
                 });
+            },
+            needShow(type, text) {
+                if (this.onlyContent && !text) {
+                    return false;
+                }
+                if (this.selectType === ALL) {
+                    return true;
+                } else {
+                    return type === this.selectType;
+                }
             }
         },
         components: {
@@ -115,6 +147,8 @@
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+    @import "../../common/stylus/mixin.styl"
+
     .food
         position: fixed
         left: 0;
@@ -217,4 +251,45 @@
                 margin-left: 18px
                 font-size: 14px
                 color: rgb(7, 17, 27)
+            .rating-wrapper
+                padding: 0 18px
+                .rating-item
+                    position: relative
+                    padding: 16px 0
+                    border-1px(rgba(7, 17, 27, 0.1))
+                    .user
+                        position: absolute
+                        right: 0
+                        top: 16px
+                        line-height: 12px
+                        font-size: 0
+                        .name
+                            display: inline-block
+                            margin-right: 6px
+                            vertical-align: top
+                            font-size: 10px
+                            color: rgb(147, 153, 159)
+                        .avatar
+                            border-radius: 50%
+                    .time
+                        margin-bottom: 6px
+                        line-height: 12px
+                        font-size: 10px
+                        color: rgb(147, 153, 159)
+                    .text
+                        line-height: 16px
+                        font-size: 12px
+                        color: rgb(7, 17, 27)
+                        .icon-thumb_up, .icon-thumb_down
+                            line-height: 16px
+                            margin-right: 4px
+                            font-size: 12px
+                        .icon-thumb_up
+                            color: rgb(0, 160, 220)
+                        .icon-thumb_down
+                            color: rgb(147, 153, 159)
+                .no-rating
+                    padding: 16px 0
+                    font-size: 12px
+                    color: rgb(147, 153, 159)
 </style>
